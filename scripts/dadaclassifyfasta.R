4f8dd571738cd34593179987d6f9ae4a534121c1
Alzbutas, Gediminas <gediminas.alzbutas@thermofisher.com>
2020-06-24 22:50:34 +0300

    Increased sensitivity

--- scripts/dadaclassifyfasta.R
+++ scripts/dadaclassifyfasta.R
@@ -1,5 +1,6 @@
 library(dada2); packageVersion("dada2")
 library(DECIPHER);
+packageVersion("dada2");
 dbin <- snakemake@params[["db"]]
 dfiltered <- snakemake@params[["dada_filt"]]
 fasta <- snakemake@input[[1]]
@@ -11,7 +12,7 @@ counter = 0
 for (s in seqs)
 {
   #limit = 250
-  limit = 260
+  limit = 1000
   l <- nchar(s)
   if ( l < limit ) {
   limit = l
@@ -37,7 +38,7 @@ taxid <- t(sapply(ids, function(x) {
 taxid <- taxid[,-7]
 colnames(taxid) <- ranks4out
 } else {
-taxid <- assignTaxonomy(seqs, dbin,minBoot=40,tryRC=TRUE,multithread=threads,verbose=TRUE)
+taxid <- assignTaxonomy(seqs, dbin,minBoot=30,tryRC=TRUE,multithread=threads,verbose=TRUE)
 }
 row.names(taxid) <- seqnames
 write.table(taxid, output_tax_table)
